8ea8defb38d68a58fbc7af2742fc9856154f4d32
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-02 16:46:56 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
- 
-   # コメントアウトする
-   # def new
-   # end
- 
+
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content])
     render json:{ post: post }
   end
- 
- end
- 
+end
